79d4773f3b7b5aa0c624071e5ffa9227dac01d09
Vladislav Pochernin <vspochernin@gmail.com>
2024-12-30 10:37:47 +0300

    initial commit for code

---
+++ src/test/java/ru/vspochernin/finance_management/FinanceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vspochernin.finance_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
