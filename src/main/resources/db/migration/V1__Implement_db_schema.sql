8948df8381f4891d269b31c31d3b668057f19376
Vladislav Pochernin <vspochernin@gmail.com>
2024-12-30 17:26:12 +0300

    implement schema

---
+++ src/main/resources/db/migration/V1__Implement_db_schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users
+(
+    id       SERIAL PRIMARY KEY,
+    login    VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255)        NOT NULL
+);
+
+CREATE TABLE categories
+(
+    id      SERIAL PRIMARY KEY,
+    user_id INTEGER      NOT NULL,
+    title    VARCHAR(255) NOT NULL,
+    type    VARCHAR(50) CHECK (type IN ('INCOME', 'EXPENSE')),
+    budget  BIGINT DEFAULT NULL,
+    UNIQUE (user_id, name),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE transactions
+(
+    id          SERIAL PRIMARY KEY,
+    category_id INTEGER NOT NULL,
+    amount      BIGINT  NOT NULL,
+    datetime    TIMESTAMP DEFAULT current_timestamp,
+    FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
+);
