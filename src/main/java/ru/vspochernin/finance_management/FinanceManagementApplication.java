d2be6b4602c2dbbd0dc1f98ad3b64233750095ec
Vladislav Pochernin <vspochernin@gmail.com>
2024-12-30 11:38:44 +0300

    implement CommandLineRunner

--- src/main/java/ru/vspochernin/finance_management/FinanceManagementApplication.java
+++ src/main/java/ru/vspochernin/finance_management/FinanceManagementApplication.java
@@ -1,13 +1,17 @@
 package ru.vspochernin.finance_management;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinanceManagementApplication {
+public class FinanceManagementApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinanceManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceManagementApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) {
+    }
 }
