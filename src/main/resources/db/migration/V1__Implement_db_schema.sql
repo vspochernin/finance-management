b70db3ab582c7c47911ed36eb6fd988ca42b481d
Vladislav Pochernin <vspochernin@gmail.com>
2024-12-30 18:19:48 +0300

    update schema

--- src/main/resources/db/migration/V1__Implement_db_schema.sql
+++ src/main/resources/db/migration/V1__Implement_db_schema.sql
@@ -8,9 +8,9 @@ CREATE TABLE users
 CREATE TABLE categories
 (
     id      SERIAL PRIMARY KEY,
-    user_id INTEGER      NOT NULL,
-    title    VARCHAR(255) NOT NULL,
-    type    VARCHAR(50) CHECK (type IN ('INCOME', 'EXPENSE')),
+    user_id INTEGER                                           NOT NULL,
+    title   VARCHAR(255)                                      NOT NULL,
+    type    VARCHAR(50) CHECK (type IN ('INCOME', 'EXPENSE')) NOT NULL,
     budget  BIGINT DEFAULT NULL,
     UNIQUE (user_id, title),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
@@ -19,8 +19,8 @@ CREATE TABLE categories
 CREATE TABLE transactions
 (
     id          SERIAL PRIMARY KEY,
-    category_id INTEGER NOT NULL,
-    amount      BIGINT  NOT NULL,
-    datetime    TIMESTAMP DEFAULT current_timestamp,
+    category_id INTEGER                             NOT NULL,
+    amount      BIGINT                              NOT NULL,
+    datetime    TIMESTAMP DEFAULT current_timestamp NOT NULL,
     FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
 );
